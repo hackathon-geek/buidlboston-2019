9ab191bb810c7729564866862fee6c76199d087f
akshitsingla <akshit.singla@yahoo.com>
2019-05-11 15:34:43 -0400

    data-schema

---
+++ ui/schema.js
@@ -0,0 +1,50 @@
+var user =  {
+    username : "",
+    inventions : {
+        "id" : {
+            // invention
+        }
+    }
+}
+
+var user_profile_page = {
+    // on-chain data
+    username : "",
+    inventions : {
+        "id" : {}
+    },
+    // off-chain plugins/data
+    social_info : {                     // nice-to-have
+        avatar : "",
+        skills : [""],
+        interests: [""],
+        following: ["username"]
+    }
+}
+
+var invention = {
+    id : "",
+    title : "",
+    version : "",
+    description : "",
+    licenses : {
+        "oracle" : "pending",
+        "oracle" : "approved"
+    }
+}
+
+var inventions_page = [{
+    // invention(s)
+}]
+
+var license_oracle = {
+    org_info : {
+        name : ""
+    },
+    inventions : {
+        "id" : {
+            status: "",
+            // invention
+        }
+    }
+}
